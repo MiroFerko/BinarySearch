81e005c6a0da609e2a19770a2de63735595176ff
MiroFerko <miroslav.ferko1@gmail.com>
2022-08-13 11:14:33 +0200

    BinarySearch

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,21 +1,24 @@
 public class BinarySearch {
     public static void main(String[] args) {
         int[] sortedArray = {1, 4, 6, 10, 22, 34, 65, 99};
-        System.out.println("Position of the number in array is " + (runBinarySearch(sortedArray, 34)+1));
+        System.out.println("Position of the number in array is " + (runBinarySearch(sortedArray, 34, 0, sortedArray.length) + 1));
     }
 
-    public static int runBinarySearch(int[] values, int x) {
-        int start = 0;
-        int end = values.length - 1;
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            if (values[mid] == x)
-                return mid;
-            else if (values[mid] < x)
-                start = mid + 1;
-            else
-                end = mid - 1;
+    public static int runBinarySearch(
+            int[] sortedArray, int key, int low, int high) {
+        int index = Integer.MAX_VALUE;
+
+        while (low <= high) {
+            int mid = low + ((high - low) / 2);
+            if (sortedArray[mid] < key) {
+                low = mid + 1;
+            } else if (sortedArray[mid] > key) {
+                high = mid - 1;
+            } else if (sortedArray[mid] == key) {
+                index = mid;
+                break;
+            }
         }
-        return -1;
+        return index;
     }
 }
