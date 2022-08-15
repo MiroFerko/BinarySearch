30a7c9b23063a65eacb044397aa22b35881bc336
MiroFerko <miroslav.ferko1@gmail.com>
2022-08-15 15:35:15 +0200

    BinarySearch

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,9 +1,12 @@
 public class BinarySearch {
+    //task- create binary search without Arrays.binarySearch() method
     public static void main(String[] args) {
         int[] sortedArray = {1, 4, 6, 10, 22, 34, 65, 99};
+        //+1 so result is according to position
         System.out.println("Position of the number in array is " + (runBinarySearch(sortedArray, 34, 0, sortedArray.length) + 1));
     }
 
+    //iterative BinarySearch method with high, low and middle positions
     public static int runBinarySearch(
             int[] sortedArray, int key, int low, int high) {
         int index = Integer.MAX_VALUE;
