270ff0cdffee058e3fb79446281ae524ba742d28
MiroFerko <miroslav.ferko1@gmail.com>
2022-08-23 20:08:47 +0200

    BinarySearch

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -6,10 +6,6 @@ public class BinarySearch {
         System.out.println("Position of the number in array is " + (runBinarySearch(sortedArray, 34, 0, sortedArray.length) + 1));
     }
 
-
-
-
-
     //iterative BinarySearch method with high, low and middle positions
     public static int runBinarySearch(
             int[] sortedArray, int key, int low, int high) {
